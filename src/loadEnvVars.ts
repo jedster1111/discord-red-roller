c50a049a8982d29fcb20ad8f93e0cfb8f05b8d36
jedster1111 <jedster1111@hotmail.co.uk>
2025-09-05 21:15:39 +0100

    Tidy up env variables

--- src/loadEnvVars.ts
+++ src/loadEnvVars.ts
@@ -2,8 +2,6 @@ import path from "node:path";
 import os from "node:os";
 
 export type EnvConfig = {
-  environment: "dev" | "prod";
-  hostName: string;
   discordBotToken: string;
   discordBotClientId: string;
   storePath: string;
@@ -14,21 +12,14 @@ const defaultStoreName = "store.json";
 const defaultStorePath = path.join(defaultStoreDirectory, defaultStoreName);
 
 export const loadEnvVars = (): EnvConfig => {
-  const environment = process.env.ENVIRONMENT || "prod";
-  const hostName = process.env.HOST_NAME;
-
   const discordBotToken = process.env.DISCORD_BOT_TOKEN;
   const discordBotClientId = process.env.DISCORD_BOT_CLIENT_ID;
 
   const storePath = process.env.STORE_PATH || defaultStorePath;
 
-  if (!hostName || !discordBotToken || !discordBotClientId) throw new Error();
-  if (!(environment === "dev" || environment === "prod"))
-    throw new Error("ENVIRONMENT variable was not either 'dev' or 'prod'!");
+  if (!discordBotToken || !discordBotClientId) throw new Error();
 
   return {
-    environment,
-    hostName,
     discordBotToken,
     discordBotClientId,
     storePath,

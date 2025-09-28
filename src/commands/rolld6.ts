e06c78a5cd31bc27b8ecf6632b5a1cb02f890ec6
jedster1111 <jedster1111@hotmail.co.uk>
2025-09-28 13:08:04 +0100

    Add optional modifiers to d6 rolls

--- src/commands/rolld6.ts
+++ src/commands/rolld6.ts
@@ -12,24 +12,39 @@ const commandJson = new SlashCommandBuilder()
   .addIntegerOption((o) =>
     o.setName("how-many-d6").setDescription("How many d6 to roll").setMaxValue(50).setMinValue(1).setRequired(true),
   )
+  .addIntegerOption((o) =>
+    o
+      .setName("modifier")
+      .setDescription("What to add or subtract to your roll")
+      .setMaxValue(1000)
+      .setMinValue(-1000)
+      .setRequired(false),
+  )
   .addStringOption((o) => o.setName("description").setDescription("Add a description to your roll").setRequired(false))
   .toJSON();
 
 const handler: CommandHandler = async (interaction) => {
   const numberDiceToRoll = interaction.options.getInteger("how-many-d6", true);
+  const modifier = interaction.options.getInteger("modifier", false) || 0;
   const rollDescription = interaction.options.getString("description", false) || "Result";
 
   const rolls = Array(numberDiceToRoll)
     .fill(0)
     .map(() => rollD6());
 
-  const result = rolls.reduce(sum);
+  const result = rolls.reduce(sum) + modifier;
 
   const stringifiedResults = rolls.map((roll) => {
     return roll === 6 ? `__${roll}__` : `${roll}`;
   });
 
-  const explanation = stringifiedResults.join(" + ");
+  let explanation = stringifiedResults.join(" + ");
+
+  if (modifier) {
+    explanation = `[${explanation}]`;
+    if (modifier > 0) explanation += ` + ${modifier}`;
+    if (modifier < 0) explanation += ` - ${-1 * modifier}`;
+  }
 
   await interaction.reply(`${rollDescription}: __**${result}**__ | *(${explanation})*`);
 };
